baaf65ef48c7155fdc418d4938c1257307a46dd6
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-28 22:53:49 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -4,6 +4,8 @@ class Artist
   attr_accessor :name
   attr_reader :songs
 
+  extend Memorable
+
   @@artists = []
 
   def self.find_by_name(name)
@@ -19,13 +21,13 @@ class Artist
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
+  # def self.reset_all
+  #   self.all.clear
+  # end
 
-  def self.count
-    @@artists.count
-  end
+  # def self.count
+  #   @@artists.count
+  # end
 
   def add_song(song)
     @songs << song
