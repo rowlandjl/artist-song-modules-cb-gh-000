ea533a6c990c0a2d5235926158e91d3d5cce3633
Jessica Rowland <rowlandjl@vcu.edu>
2018-07-28 22:51:24 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -20,9 +20,9 @@ class Song
     @artist = artist
   end
 
-  def self.reset_all
-    @@songs.clear
-  end
+  # def self.reset_all
+  #   @@songs.clear
+  # end
 
   def to_param
     name.downcase.gsub(' ', '-')
